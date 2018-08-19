e923a10135877a8721aa40aed9df7c7f745082ac
Jaeyu Jo <ehay@naver.com>
2018-08-19 19:53:23 +0900

    note

---
+++ contents/src/chapter03/section04/TestMain.java
@@ -0,0 +1,15 @@
+package chapter03.section04;
+
+/**
+ * Created by ehay@naver.com on 2018-08-19
+ * Blog : http://ehay.tistory.com
+ * Github : http://github.com/ehayand
+ */
+
+public class TestMain {
+
+    //람다식
+    public static void main(String[] args){
+
+    }
+}
---
+++ contents/src/chapter04/section05/TestMain.java
@@ -0,0 +1,15 @@
+package chapter04.section05;
+
+/**
+ * Created by ehay@naver.com on 2018-08-19
+ * Blog : http://ehay.tistory.com
+ * Github : http://github.com/ehayand
+ */
+
+public class TestMain {
+
+    //리플렉션
+    public static void main(String[] args){
+
+    }
+}
